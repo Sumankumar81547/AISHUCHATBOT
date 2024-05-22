beacb40cad230ef7f473fe28144147b27603b85c
♦️⃟≛⃝𝑼𝑵𝑰𝑸𝑼𝑬⃟♥️⃟𝑩𝑶𝒀⃝≛⁤⃟♦️ <168259000+Sumankumar81547@users.noreply.github.com>
2024-05-22 11:57:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
+OWNER_ID = "6993831753"
 SUPPORT_GRP = "R_I_S_I_NG"
 UPDATE_CHNL = "ROYAL_WORLD_84"
-OWNER_USERNAME = "iam_daxx"
+OWNER_USERNAME = "Moon_x_22"
 
